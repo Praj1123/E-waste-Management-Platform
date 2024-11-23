5b8f7c3bb86dd6d1616c5870c2c4d7a21b38b03c
Praj1123 <prajwalchaudhary663@gmail.com>
2024-11-23 15:38:15 +0530

    updated app.py

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ def payment_success():
 app.secret_key = "hellow_EcoRecycle"
 try:
     # Connect to the MongoDB server
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient("mongodb+srv://prajwalchaudhari762:CBXSxTBk5NUvgHkN@cluster0.r0i8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    #mongodb://localhost:27017/
     # Check the connection by retrieving the server info
     client.server_info()  # This will raise an exception if the connection fails
 
